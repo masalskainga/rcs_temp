bfd281f9e959848e63a3d7e768957b27a0324d99
masalskainga <141061252+masalskainga@users.noreply.github.com>
2023-10-09 11:03:01 +0300

    Add files via upload

---
+++ mdokt6/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        mdokt6();
+}
+
+    public static void mdokt6() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("&");
+            for (int j = 0; j <= i; j++) {
+                System.out.println("&");
+            }
+        }
+    }
