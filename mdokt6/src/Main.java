6e078685acab39d09adbd745386c9412d1fe1263
masalskainga <masalskainga@gmail.com>
2023-10-13 23:09:03 +0300

    Update Main.java

--- mdokt6/src/Main.java
+++ mdokt6/src/Main.java
@@ -8,23 +8,30 @@ public class Main {
 
 
 public static void mdokt6(){
-    int number = 5;
-    for (int i = 0; i <= number; i++){
-        System.out.println("&");
+    String sum = "&";
+    System.out.println(sum);
+    for (int i = 0; i < 3; i++){
+        sum = sum + "&";
+        System.out.println(sum);
     }
-       System.out.println();
+
 }
 public static void mdokt11() {
     Scanner sc = new Scanner(System.in);
     System.out.println("Lūdzu, norādi, cik frāzes vēlies ievadīt.");
     int number = sc.nextInt();
     String ievade = sc.nextLine();
+
+    int garaka = 0;
     for (int i = 0; i < number; i++) {
         System.out.println("Lūdzu, ievadi frāzi.");
         String fraze = sc.nextLine();
-
+        garaka = garaka + fraze.length();
+        if(fraze.length > garaka){
+            System.out.println(garaka);
+        }
     }
-    String fraze = ievade.length();
+
     }
 
 }
