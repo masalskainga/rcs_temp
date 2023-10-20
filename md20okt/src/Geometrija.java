3a2a5251c5c0c281453b884a85924fbc0dd8c6b6
masalskainga <masalskainga@gmail.com>
2023-10-20 20:24:20 +0300

    majasdarbs20okt

---
+++ md20okt/src/Geometrija.java
@@ -0,0 +1,11 @@
+public class Geometrija {
+    public static int Trijsturis(int a, int b, int c) {
+        int sum = a + b + c;
+        return sum;
+    }
+    public static int Cetrsturis(int a, int b, int c, int d){
+        int sum = a + b + c + d;
+        return sum;
+    }
+
+}
---
+++ md20okt/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int sum  = Geometrija.Trijsturis(4,6, 8);
+//        System.out.println(sum);
+
+        int sum = Geometrija.Cetrsturis(4, 6, 4, 6);
+        System.out.println(sum);
+    }
+
+
+}
