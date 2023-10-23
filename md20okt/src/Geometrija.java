9607ff84a8a4500f4815711a5f16aaab4c5bf8ee
masalskainga <masalskainga@gmail.com>
2023-10-23 13:31:06 +0300

    papildinats md

--- md20okt/src/Geometrija.java
+++ md20okt/src/Geometrija.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Geometrija {
     public static int Trijsturis(int a, int b, int c) {
         int sum = a + b + c;
@@ -8,4 +10,31 @@ public class Geometrija {
         return sum;
     }
 
+    public static double triangle(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi pirmās malas izmēru:");
+        double side1 = scanner.nextDouble();
+
+        System.out.println("Ievadi otrās malas izmēru: ");
+        double side2 = scanner.nextDouble();
+
+        System.out.println("Ievadi trešās malas izmēru: ");
+        double side3 = scanner.nextDouble();
+
+        double perimeter = side1 + side2 + side3;
+
+        System.out.println("Trijstūra perimetrs ir: " + perimeter);
+
+        return perimeter;
+
+    }
+
+    public static double rectangle(double side1, double side2, double side3, double side4) {
+        double perimeter = side1 + side2 + side3 + side4;
+
+        System.out.println("Četrstūra perimetrs ir: " + perimeter);
+
+        return perimeter;
+    }
 }
--- md20okt/src/Main.java
+++ md20okt/src/Main.java
@@ -4,8 +4,15 @@ public class Main {
 //        int sum  = Geometrija.Trijsturis(4,6, 8);
 //        System.out.println(sum);
 
-        int sum = Geometrija.Cetrsturis(4, 6, 4, 6);
-        System.out.println(sum);
+//        int sum = Geometrija.Cetrsturis(4, 6, 4, 6);
+//        System.out.println(sum);
+
+//        double perimeter = Geometrija.triangle();
+//        System.out.println(perimeter);
+
+        double perimeter = Geometrija.rectangle(7, 8, 7, 8);
+        System.out.println(perimeter);
+
     }
 
 
